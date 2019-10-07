817b60b0935f468b0b8137f916d361e8285ed81a
Jay Amin <jayamin@Jays-MacBook-Pro.local>
2019-10-06 22:13:22 -0400

    first commit

---
+++ wikipediaScrapper.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+import re
+r = requests.get('https://en.wikipedia.org/wiki/Thomas_Jefferson')
+type(r)
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+table = soup.find('table', {'class': 'infobox vcard'}).tbody
+line = str(table.text)
+for word in line:
+    for letter in word:
+        if letter is int or letter.upper():
+            print('\n')
+print(line)
+
+
+
